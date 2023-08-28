36edac9b7162a505a365013f0680d2de2725e1a7
Philipp Leitenbauer <37079522+msftphleiten@users.noreply.github.com>
2023-08-28 16:12:14 +0200

    initial version

---
+++ azure-storage-performance.js
@@ -0,0 +1,35 @@
+function displayPerformance() {
+
+    GiB = document.getElementById("id_GiB").value;
+
+    IOPS = 3000 + 1 * GiB;
+    IOPS_Burst = Math.min(Math.max(10000, 3 * GiB), 100000);
+    Throughput = 100 + Math.ceil(0.04 * GiB) + Math.ceil(0.06 * GiB);
+
+    var newTable = "<table border='1'>";
+    
+    newTable += "<tr>";
+    newTable += "<td>GiB</td>";
+    newTable += "<td class='alnright'>" + GiB + "</td>";
+    newTable += "</tr>";
+
+    newTable += "<tr>";
+    newTable += "<td>Max. IOPS</td>";
+    newTable += "<td class='alnright'>" + IOPS + "</td>";
+    newTable += "</tr>";
+    
+    newTable += "<tr>";
+    newTable += "<td>Max. IOPS Burst</td>";
+    newTable += "<td class='alnright'>" + IOPS_Burst + "</td>";
+    newTable += "</tr>";
+
+    newTable += "<tr>";
+    newTable += "<td>Max. Throughput</td>";
+    newTable += "<td class='alnright'>" + Throughput + "</td>";
+    newTable += "</tr>";
+
+    newTable += "</table>";
+
+    document.getElementById("idCalculation").innerHTML = newTable;
+    
+}
